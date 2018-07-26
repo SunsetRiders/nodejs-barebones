4b6fd34b0a8f3deae9b17958d6269da64156bb2b
vinacovre <viniassis@me.com>
2018-07-26 14:42:33 -0300

    update

--- bin/app.js
+++ bin/app.js
@@ -1,7 +1,7 @@
 const config = require('getconfig');
 const Logger = require('logger');
 const expressXRequestId = require('express-x-request-id');
-const expressRoutes = require('../src/routes');
+const routes = require('../src/routes');
 require('dotenv').config({silent: true});
 
 const express = require('express');
@@ -11,9 +11,14 @@ app.use(expressXRequestId.requestMiddleware);
 app.use(expressXRequestId.responseMiddleware);
 app.use(Logger.injectLogger());
 app.use(Logger.injectRequestLogger());
-app.use(expressRoutes);
+app.use(routes);
 app.use((req, res, next) => res.sendStatus(404));
 
 const logger = Logger.newInstance();
 const port = config.port;
-app.listen(port, () => logger.info(`Listening on port ${port}...`));
+app.listen(port, () => {
+  logger.info(`Listening on port ${port}...`);
+}).on('error', err => {
+  logger.error(`Startup ${err}`);
+  process.exit(1);
+});
