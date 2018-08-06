4539b467d41768b7be2b834fdd9a6068a521eefc
vinacovre <viniassis@me.com>
2018-08-06 10:53:31 -0300

    add body parser

--- config/local.js
+++ config/local.js
@@ -1,5 +1,6 @@
 module.exports = {
   environment: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-  apiKey: process.env.API_KEY || 'fallback_api_key'
+  apiKey: process.env.API_KEY || 'fallback_api_key',
+  payloadSize: process.env.PAYLOAD_SIZE_LIMIT || '1mb'
 };
